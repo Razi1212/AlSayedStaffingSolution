da2fc208e308122a2d5366f8d7f4e6021dc371c4
Raziuddin Shah <farooqui.razi8700@gmail.com>
2025-04-14 22:03:38 +0530

    Changed Employers

--- src/pages/Employers/index.jsx
+++ src/pages/Employers/index.jsx
@@ -24,10 +24,10 @@ const Industries = () => {
 
           <h2 className="text-xl font-semibold mb-4">We proudly support the following industries:</h2>
 
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-6 text-white">
+          <div className="text-black ">
             {/* Oil & Gas */}
-            <div className="w-full">
-              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+            <div className="w-full lg:flex">
+              <div>
                 <h6 className="text-lg font-semibold mb-2">Oil & Gas</h6>
                 <p className="text-sm leading-relaxed mb-4">
                   The Oil & Gas industry demands highly skilled and experienced professionals to handle complex projects, ensure safety, and maintain efficiency in a challenging work environment.
@@ -47,12 +47,23 @@ const Industries = () => {
                     <li>✔️ Insulation Worker</li>
                   </ul>
                 </div>
-              </SpotlightCard>
+
+              </div>
+
+              <div>
+                <img src="./AboutHero2.webp" alt="Sample" className="w-full object-cover" />
+              </div>
+
             </div>
 
             {/* Construction */}
-            <div className="w-full">
-              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+            <div className="w-full lg:flex">
+
+              <div>
+                <img src="./AboutHero2.webp" alt="Sample" className="w-full object-cover" />
+              </div>
+
+              <div>
                 <h6 className="text-lg font-semibold mb-2">Construction</h6>
                 <p className="text-sm leading-relaxed mb-4">
                   The construction industry is the backbone of infrastructure development. It requires a workforce with technical skills, physical strength, and a strong understanding of building codes and safety regulations.
@@ -72,12 +83,13 @@ const Industries = () => {
                     <li>✔️ Insulation Worker</li>
                   </ul>
                 </div>
-              </SpotlightCard>
+              </div>
+
             </div>
 
             {/* Healthcare */}
-            <div className="w-full">
-              <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+            <div className="w-full lg:flex">
+              <div>
                 <h6 className="text-lg font-semibold mb-2">Healthcare</h6>
                 <p className="text-sm leading-relaxed mb-4">
                   Healthcare is a critical industry that requires highly trained and compassionate professionals to provide quality patient care.
@@ -98,8 +110,105 @@ const Industries = () => {
                     <li>✔️ Insulation Worker</li>
                   </ul>
                 </div>
-              </SpotlightCard>
+
+              </div>
+
+              <div>
+                <img src="./AboutHero2.webp" alt="Sample" className="w-full object-cover" />
+              </div>
             </div>
+
+
+            <p>others industries that we support</p>
+
+            <div className="lg:flex gap-20 m-9">
+
+              <div className="border-red-500 border-2 pb-10 rounded-lg shadow-md w-full sm:w-[300px] hover:bg-red-500 hover:text-white">
+              <img src="./construction.jpg" alt="Sample" className="w-full object-cover rounded-b-[100px] rounded-lg " />
+                <h3 className="text-lg font-semibold mb-4">Hi-Tech</h3>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ Product Engineering</li>
+                  <li>✔️ Product Testing (Automated)</li>
+                  <li>✔️ Mobility</li>
+                  <li>✔️ Internet Of Things</li>
+                </ul>
+              </div>
+
+              <div className=" border-blue-500 border-2 pb-10 rounded-lg shadow-md w-full sm:w-[300px] hover:bg-blue-500 hover:text-white">
+              <img src="./construction.jpg" alt="Sample" className="w-full object-cover rounded-b-[100px] rounded-lg " />
+                <h6 className="text-lg font-semibold mb-4"> Banking & Finance</h6>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ Core Banking Application Implementation</li>
+                  <li>✔️ Interface Development</li>
+                  <li>✔️ Products (Govt .Business Framework)</li>
+                </ul>
+              </div>
+
+              <div className=" border-yellow-500 border-2 pb-10 rounded-lg shadow-md w-full sm:w-[300px] hover:bg-yellow-500 hover:text-white" >
+              <img src="./construction.jpg" alt="Sample" className="w-full object-cover rounded-b-[100px] rounded-lg " />
+                <h6 className="text-lg font-semibold mb-4">Retail & E-commerce</h6>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ E-commerce Enablement</li>
+                  <li>✔️ Product Catalog Systems/Warehouse Systems</li>
+                  <li>✔️ Enterprise Mobility</li>
+                  <li>✔️ Cloud Native Development</li>
+                </ul>
+              </div>
+
+
+            </div>
+
+
+            <div className="lg:flex gap-20 m-9">
+
+              <div className=" border-green-500 border-2 pb-10 rounded-lg shadow-md w-full sm:w-[300px] hover:bg-bg:green-500 hover:text-white ">
+              <img src="./construction.jpg" alt="Sample" className="w-full object-cover rounded-b-[100px] rounded-lg " />
+                <h6 className="text-lg font-semibold mb-4">Consumer Electronics</h6>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ Networking</li>
+                  <li>✔️ Embedded</li>
+                  <li>✔️ Test Automation</li>
+                  <li>✔️ Firmware Development</li>
+                  <li>✔️ Product Engineering</li>
+                </ul>
+              </div>
+
+
+              <div className="border-amber-500 border-2 pb-10  rounded-lg shadow-md w-full sm:w-[300px] hover:bg-amber-500 hover:text-white">
+              <img src="./construction.jpg" alt="Sample" className="w-full object-cover rounded-b-[100px] rounded-lg " />
+                <h6 className="text-lg font-semibold mb-4">Consumer Electronics</h6>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ Networking</li>
+                  <li>✔️ Embedded</li>
+                  <li>✔️ Test Automation</li>
+                  <li>✔️ Firmware Development</li>
+                  <li>✔️ Product Engineering</li>
+                </ul>
+              </div>
+
+              <div className="border-teal-500 border-2 pb-10  rounded-lg shadow-md w-full sm:w-[300px] hover:bg-teal-500 hover:text-white">
+              <img src="./construction.jpg" alt="Sample" className="w-full object-cover rounded-b-[100px] rounded-lg " />
+                <h6 className="text-lg font-semibold mb-4">Automotive and Manufacturing</h6>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ Customer Development and Maintence</li>
+                  <li>✔️ Application Migration (Cloud)</li>
+                  <li>✔️ Mobility</li>
+                  <li>✔️ Testing & QA/Devpos</li>
+                </ul>
+              </div>
+
+
+              {/* <div>
+                <h6>HealthCare & Life Sciences</h6>
+                <ul className="space-y-2 text-sm">
+                  <li>✔️ Customer Application</li>
+                  <li>✔️ Plaform Based Development</li>
+                  <li>✔️ BigDate Analytics</li>
+                  <li>✔️ Patient Care (products & Framework)</li>
+                </ul>
+              </div> */}
+            </div>
+
           </div>
 
         </div>
