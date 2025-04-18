445f3e361a0becf1dd72915d866dca2c9dfac88a
Razi1212 <farooqui.razi8700@gmail.com>
2025-04-18 16:50:58 +0530

    added spaces

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ export default function () {
           playsInline
           className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
         >
-          <source src="./video2.mp4" type="video/mp4" />
+          <source src="./video3.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
 
@@ -31,8 +31,10 @@ export default function () {
           </p>
         </div>
       </div>
+
+
       {/* <section className="bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1]  px-8 py-6 lg:px-[5%] lg:py-[5%] xl:px-[8%] xl:py-[8%]"> */}
-      <section className="bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1]  lg:p-[8%] p-5">
+      <section className="bg-gradient-to-bl from-[#ffe4e6] to-[#ccfbf1]  lg:p-[8%] px-5 py-14">
         <div className="flex flex-col lg:flex-row items-center justify-between space-y-6 ">
           <div className="flex-1 space-y-4">
             <h6 className="text-3xl font-bold text-gray-900">
@@ -109,7 +111,7 @@ export default function () {
       </section>
 
       <section className=" bg-BgColor-homecolor font-geist">
-        <div className="lg:flex lg:p-[8%]">
+        <div className="lg:flex lg:p-[8%] py-14">
           <div className="flex-1">
             <img
               src="./AboutHero22.png"
@@ -140,6 +142,7 @@ export default function () {
           </div>
         </div>
       </section>
+
 
       <section className="bg-cover bg-center bg-no-repeat py-16 px-8  flex items-center justify-center bg-BgColor-homecolor">
         <div className="relative z-10 flex flex-col md:flex-row items-center gap-10 max-w-6xl mx-auto">
@@ -183,6 +186,34 @@ export default function () {
               alt="Professional team working together"
               className="w-full h-auto object-cover"
             />
+          </div>
+        </div>
+      </section>
+
+      <section className=" bg-BgColor-homecolor font-white py-14">
+        <div className="lg:flex lg:p-[8%] ">
+          <div className="flex-1">
+            <img
+              src="./AboutHero22.png"
+              alt="Description of the image"
+              className="w-full h-auto object-contain 
+             max-h-[200px] sm:max-h-[400px] md:max-h-[400px] lg:max-h-[500px] xl:max-h-[500px]"
+            />
+          </div>
+
+          <div className="flex-1  py-10 px-6">
+            <h3 className="text-3xl font-bold text-gray-800  lg:text-gray-900 ">
+              From Search to Success — We’ve Got You Covered
+            </h3>
+            <p className="mt-4 text-gray-900 md:text-lg text-base">
+              Looking to hire top talent or streamline your recruitment process? Our consultancy services are tailored to meet your hiring needs. We partner with companies across industries to deliver customized staffing solutions, saving you time and resources. Whether you're scaling quickly or searching for niche skills, our team is here to support your growth.
+            </p>
+
+            <Link href="/Employers">
+              <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                Go to Employers
+              </button>
+            </Link>
           </div>
         </div>
       </section>
