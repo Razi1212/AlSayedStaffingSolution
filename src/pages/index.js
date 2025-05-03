ead32a66e8fc943942c1e568cb231fc1f68affe4
Raziuddin Shah <farooqui.razi8700@gmail.com>
2025-05-03 19:17:15 +0530

    Added Animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,6 +12,12 @@ export default function () {
   const ref2 = useRef(null);
   const isInView2 = useInView(ref2, { once: true });
 
+  const ref3 = useRef(null);
+  const isInView3 = useInView(ref3, { once: true });
+
+  const ref4 = useRef(null);
+  const isInView4 = useInView(ref4, { once: true });
+
   const fadeInUp = {
     hidden: { opacity: 0, y: 40 },
     visible: { opacity: 1, y: 0 },
@@ -139,7 +145,7 @@ export default function () {
 
       <section className=" bg-BgColor-homecolor font-geist">
         <div className="lg:flex lg:p-[8%] py-14">
-        <motion.div
+          <motion.div
             ref={ref2}
             variants={fadeInUp}
             initial="hidden"
@@ -147,15 +153,15 @@ export default function () {
             transition={{ duration: 1.2 }}
             className="flex-1"
           >
-          <div className="flex-1">
-            <img
-              src="./AboutHero22.png"
-              alt="Description of the image"
-              // className="lg:w-full lg:h-auto max-h-[500px]"
-              className="w-full h-auto object-contain 
+            <div className="flex-1">
+              <img
+                src="./AboutHero22.png"
+                alt="Description of the image"
+                // className="lg:w-full lg:h-auto max-h-[500px]"
+                className="w-full h-auto object-contain 
              max-h-[200px] sm:max-h-[400px] md:max-h-[400px] lg:max-h-[500px] xl:max-h-[500px]"
-            /> 
-          </div>
+              />
+            </div>
           </motion.div>
 
           <motion.div
@@ -165,88 +171,122 @@ export default function () {
             transition={{ duration: 1.2, delay: 0.3 }}
             className="flex-1 py-10 px-6"
           >
-          <div className="flex-1  py-10 px-6">
-            <h3 className="text-3xl font-bold text-gray-800  lg:text-gray-900 ">
-              Connecting Regional Talent with Gulf Opportunities.
-            </h3>
-            <p className="mt-4 text-gray-900 md:text-lg text-base">
-              We specialize in placing skilled professionals across key Gulf
-              industries, including oil and gas, construction, manufacturing,
-              finance, retail, hospitality, IT, and healthcare. Our tailored
-              recruitment solutions bridge the gap between top talent and
-              leading companies.
-            </p>
+            <div className="flex-1  py-10 px-6">
+              <h3 className="text-3xl font-bold text-gray-800  lg:text-gray-900 ">
+                Connecting Regional Talent with Gulf Opportunities.
+              </h3>
+              <p className="mt-4 text-gray-900 md:text-lg text-base">
+                We specialize in placing skilled professionals across key Gulf
+                industries, including oil and gas, construction, manufacturing,
+                finance, retail, hospitality, IT, and healthcare. Our tailored
+                recruitment solutions bridge the gap between top talent and
+                leading companies.
+              </p>
 
-            <Link href="/Services">
-              <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
-                Go to Services
-              </button>
-            </Link>
-          </div>
+              <Link href="/Services">
+                <button className="mt-6 px-6 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600">
+                  Go to Services
+                </button>
+              </Link>
+            </div>
           </motion.div>
         </div>
-        
       </section>
-      
 
       <section className="bg-cover bg-center bg-no-repeat py-16 px-8  flex items-center justify-center bg-BgColor-homecolor">
         <div className="relative z-10 flex flex-col md:flex-row items-center gap-10 max-w-6xl mx-auto">
           {/* Left Content */}
           <div className="text-center md:text-left max-w-2xl">
-            <h6 className="lg:text-4xl xl:text-3xl md:text-4xl font-bold text-gray-900">
-              Let’s Collaborate with Us!
-            </h6>
-            <p className="mt-4 text-gray-700 leading-relaxed lg:text-xl">
-              Looking for a job or seeking top talent to join your team? Whether
-              you're a job seeker searching for the perfect opportunity or an
-              employer looking to hire skilled professionals, we've got you
-              covered! Browse career opportunities, or get in touch with us for
-              personalized assistance and more details. Let's build successful
-              careers and teams together!
-            </p>
+            <motion.div
+              ref={ref3}
+              variants={fadeInUp}
+              initial="hidden"
+              animate={isInView3 ? "visible" : "hidden"}
+              transition={{ duration: 1.2 }}
+              className="flex-1 space-y-4"
+            >
+              <h6 className="lg:text-4xl xl:text-3xl md:text-4xl font-bold text-gray-900">
+                Let’s Collaborate with Us!
+              </h6>
+              <p className="mt-4 text-gray-700 leading-relaxed lg:text-xl">
+                Looking for a job or seeking top talent to join your team?
+                Whether you're a job seeker searching for the perfect
+                opportunity or an employer looking to hire skilled
+                professionals, we've got you covered! Browse career
+                opportunities, or get in touch with us for personalized
+                assistance and more details. Let's build successful careers and
+                teams together!
+              </p>
 
-            <div className="mt-6 flex flex-col md:flex-row gap-4 lg:text-xl">
-              <Link href="/Jobseekers">
-                <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-700 transition">
-                  Find a Job
-                </button>
-              </Link>
-              <Link href="/Employers">
-                <button className="bg-green-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-green-700 transition">
-                  Hire Talent
-                </button>
-              </Link>
-              <Link href="/Contactus">
-                <button className="bg-gray-800 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-gray-900 transition">
-                  Contact Us
-                </button>
-              </Link>
-            </div>
+              <div className="mt-6 flex flex-col md:flex-row gap-4 lg:text-xl">
+                <Link href="/Jobseekers">
+                  <button className="bg-blue-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-blue-700 transition">
+                    Find a Job
+                  </button>
+                </Link>
+                <Link href="/Employers">
+                  <button className="bg-green-600 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-green-700 transition">
+                    Hire Talent
+                  </button>
+                </Link>
+                <Link href="/Contactus">
+                  <button className="bg-gray-800 text-white px-6 py-3 rounded-lg shadow-lg hover:bg-gray-900 transition">
+                    Contact Us
+                  </button>
+                </Link>
+              </div>
+            </motion.div>
           </div>
 
           {/* Right Image */}
           <div className="max-w-md md:max-w-lg w-full">
-            <img
-              src="./Carrer1.png"
-              alt="Professional team working together"
-              className="w-full h-auto object-cover"
-            />
+            <motion.div
+              variants={fadeInUp}
+              initial="hidden"
+              animate={isInView3 ? "visible" : "hidden"}
+              transition={{ duration: 1.2, delay: 0.3 }}
+              className="flex-1"
+            >
+              <img
+                src="./Carrer1.png"
+                alt="Professional team working together"
+                className="w-full h-auto object-cover"
+              />
+            </motion.div>
           </div>
         </div>
       </section>
 
       <section className=" bg-BgColor-homecolor font-white py-14">
         <div className="lg:flex lg:p-[8%] ">
+       
           <div className="flex-1">
+          <motion.div
+              ref={ref4}
+              variants={fadeInUp}
+              initial="hidden"
+              animate={isInView4 ? "visible" : "hidden"}
+              transition={{ duration: 1.2 }}
+              className="flex-1 space-y-4"
+            >
             <img
               src="./AboutHero22.png"
               alt="Description of the image"
               className="w-full h-auto object-contain 
              max-h-[200px] sm:max-h-[400px] md:max-h-[400px] lg:max-h-[500px] xl:max-h-[500px]"
             />
+          </motion.div>
           </div>
 
           <div className="flex-1  py-10 px-6">
+          <motion.div
+              ref={ref4}
+              variants={fadeInUp}
+              initial="hidden"
+              animate={isInView4 ? "visible" : "hidden"}
+              transition={{ duration: 1.2 }}
+              className="flex-1 space-y-4"
+            >
             <h3 className="text-3xl font-bold text-gray-800  lg:text-gray-900 ">
               From Search to Success — We’ve Got You Covered
             </h3>
@@ -264,11 +304,15 @@ export default function () {
                 Go to Employers
               </button>
             </Link>
+            </motion.div>
           </div>
+      
         </div>
+
       </section>
 
       <section className="lg:p-[8%]">
+
         <h6 className="text-xl font-semibold text-gray-800">
           Insights from the People We've Placed
         </h6>
