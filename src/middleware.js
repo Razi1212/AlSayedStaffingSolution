d7be93052ec1c75735af90309b1cb92964b87631
Raziuddin Shah <farooqui.razi8700@gmail.com>
2025-07-26 23:49:24 +0530

    middle ware changes

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,33 @@
-import { NextResponse } from 'next/server';
-import cookie from 'cookie';
+// import { NextResponse } from 'next/server';
+// import cookie from 'cookie';
 
+// export function middleware(req) {
+//   const { pathname } = new URL(req.url);
+
+
+
+//   // Protect Admin routes
+//   if (pathname.startsWith('/Admin')) {
+//     const cookies = cookie.parse(req.headers.get('cookie') || '');
+//     const token = cookies.isAuthenticated;
+
+//     console.log('Token:', token);
+
+//     if (!token) {
+//       console.log('No token found. Redirecting to /login...');
+//       return NextResponse.redirect(new URL('/login', req.url));
+//     }
+//   }
+
+//   return NextResponse.next();
+// }
 export function middleware(req) {
   const { pathname } = new URL(req.url);
 
-
-
-  // Protect Admin routes
   if (pathname.startsWith('/Admin')) {
-    const cookies = cookie.parse(req.headers.get('cookie') || '');
-    const token = cookies.isAuthenticated;
-
-    console.log('Token:', token);
+    const token = req.cookies.get('isAuthenticated')?.value;
 
     if (!token) {
-      console.log('No token found. Redirecting to /login...');
       return NextResponse.redirect(new URL('/login', req.url));
     }
   }
